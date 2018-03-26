2b5869e2c299094e123ffbe2303c10da71619c07
AlekdandrNsk <aleksparshukov@yandex.ru>
2018-03-27 01:54:15 +0700

    HW04 optional

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
@@ -47,7 +47,7 @@ public class JpaMealRepositoryImpl implements MealRepository {
     @Override
     public Meal get(int id, int userId) {
         Meal meal = em.find(Meal.class, id);
-        return meal.getUser().getId() == userId ? meal : null ;
+        return meal.getUser().getId() == userId ? meal : null;
     }
 
     @Override
