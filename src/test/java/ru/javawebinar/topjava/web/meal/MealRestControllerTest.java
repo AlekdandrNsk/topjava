450b4de6fe90736970902904d47420c22a29cb7b
AlekdandrNsk <aleksparshukov@yandex.ru>
2018-04-25 05:51:53 +0700

    HW01 base 2.1

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,32 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.Test;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+
+
+public class MealRestControllerTest extends AbstractControllerTest{
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void getBetween() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void createWithLocation() {
+    }
+}
